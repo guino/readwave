7239773cee28f6a925ec248a4848df3cd247a9ac
wagner.oliveira <wagner.oliveira@fireking.com>
2021-02-07 11:30:29 -0500

    Initial files

---
+++ read.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+DATA=$(python /home/pi/airthings/read_wave2.py 2950010920 30)
+HUM=$(echo $DATA | awk '{print $2}')
+TP=$(echo $DATA | awk '{print $5}')
+RAD=$(echo $DATA | awk '{print $9}')
+echo R=$RAD T=$TP H=$HUM
+if [ "$RAD" != "" ]; then
+ wget -O- "http://10.10.10.222:8080/json.htm?type=command&param=udevice&idx=74&nvalue=0&svalue=$RAD" >/dev/null 2>&1
+fi
+if [ "$TP" != "" ]; then
+ wget -O- "http://10.10.10.222:8080/json.htm?type=command&param=udevice&idx=72&nvalue=0&svalue=$TP" >/dev/null 2>&1
+fi
+if [ "$HUM" != "" ]; then
+ wget -O- "http://10.10.10.222:8080/json.htm?type=command&param=udevice&idx=73&nvalue=$HUM&svalue=1" >/dev/null 2>&1
+fi
